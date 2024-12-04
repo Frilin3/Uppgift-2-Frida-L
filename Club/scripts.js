bc1b1a4f090fcc589c8738df30c4598b166aa30d
Frilin3 <frida.lindkvist@gritacademy.se>
2024-12-04 17:10:29 +0100

    Add files via upload

---
+++ Club/scripts.js
@@ -0,0 +1,21 @@
+
+let vip = true
+
+let age = 17
+ 
+
+
+if(true) {
+    alert("Access granted")
+
+} else {
+    alert("Access denied")
+}
+
+if(age >= 18) {
+    console.log(true)
+}
+
+if(vip=true) {
+    console.log("Welcome in my best VIP customer :)")
+}
